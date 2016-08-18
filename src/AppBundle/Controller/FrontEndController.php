e0690d50600076a5baeb96918013da541c8a137a
ihorkruchynenko <ihorkruchynenko@10.0.0.7>
2016-08-18 22:18:18 +0200

    Great commit

--- src/AppBundle/Controller/FrontEndController.php
+++ src/AppBundle/Controller/FrontEndController.php
@@ -183,7 +183,7 @@ class FrontEndController extends Controller
         if(!$newOptInDetails) {
             throw $this->createNotFoundException('U bettr go awai!');
         } else {
-            $newOptInDetails = $em ->getRepository('AppBundle:SubscriberOptInDetails') ->findOneBy(['user' => $userid]);
+            $newOptInDetails = $em ->getRepository('AppBundle:SubscriberOptInDetails') ->findOneBy(['user' => $userid, 'resourceid' => 5]);
             $newOptInDetails ->setOptindate(new DateTime());
             $newOptInDetails ->setOptinip($_SERVER['REMOTE_ADDR']);
             $em->persist($newOptInDetails);
